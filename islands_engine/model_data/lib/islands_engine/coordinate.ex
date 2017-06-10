38ef8803538c6cf8fb7fbf23afd4fc5c9ffe4aab
Arafat M <github@arafatm.com>
2017-06-09 17:33:29 -0700

    defstruct[:row, :col] with @enforce_keys to ensure both attributes are present

--- islands_engine/model_data/lib/islands_engine/coordinate.ex
+++ islands_engine/model_data/lib/islands_engine/coordinate.ex
@@ -3,4 +3,6 @@ defmodule IslandsEngine.Coordinate do
   # So we can use it as %Coordinate{} instead of %IslandsEngine.Coordinate
   alias __MODULE__ 
 
+  @enforce_keys [:row, :col] # must come before defstruct
+  defstruct [:row, :col]
 end
