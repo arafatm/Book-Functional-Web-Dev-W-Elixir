351c021ea036dd0f0a69584296e91f9f6990a074
Arafat M <github@arafatm.com>
2017-06-09 17:37:45 -0700

    Coordinate.new with row/col validation they are within 1..10

--- islands_engine/model_data/lib/islands_engine/coordinate.ex
+++ islands_engine/model_data/lib/islands_engine/coordinate.ex
@@ -5,4 +5,15 @@ defmodule IslandsEngine.Coordinate do
 
   @enforce_keys [:row, :col] # must come before defstruct
   defstruct [:row, :col]
+
+  # row, col are 10 wide
+  @board_range 1..10
+
+  # Add a new coordinate if range is valid
+  def new(row, col)  when row in(@board_range) and col in(@board_range), do:
+  {:ok, %Coordinate{row: row, col: col}}
+
+  # Catch invalid range for row/col
+  def new(_row, _col), do: {:error, :invalid_coordinate}
+
 end
