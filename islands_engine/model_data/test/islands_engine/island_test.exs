12efc401bba4d698532630e58c867a12ab802fef
Arafat M <github@arafatm.com>
2017-06-10 18:37:10 -0700

    IslandsEngine.Island create Islands with defined shapes

--- islands_engine/model_data/test/islands_engine/island_test.exs
+++ islands_engine/model_data/test/islands_engine/island_test.exs
@@ -5,31 +5,64 @@ defmodule IslandTest do
 
   alias IslandsEngine.{ Coordinate, Island }
 
-  test "has coordinates" do
-    isl = Island.new
-    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(2,4), 1)))
-    assert MapSet.size(isl.coordinates) == 2
+  test "can create :atoll" do
+    isl = Island.new(:atoll, elem(Coordinate.new(4,6), 1))
+    assert {:ok, %Island{coordinates: coordinates, hit_coordinates: _}} = isl
+    assert MapSet.size(coordinates) > 0
   end
 
-  test "has hit_coordinates" do
-    isl = Island.new
-    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(2,4), 1)))
-    assert MapSet.size(isl.hit_coordinates) == 2
+  test "can create :dot" do
+    isl = Island.new(:dot, elem(Coordinate.new(4,6), 1))
+    assert {:ok, %Island{coordinates: coordinates, hit_coordinates: _}} = isl
+    assert MapSet.size(coordinates) > 0
   end
 
-  test "cannot duplicate a coordinate" do
-    isl = Island.new
-    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    assert MapSet.size(isl.coordinates) == 1
+  test "can create :l_shape" do
+    isl = Island.new(:l_shape, elem(Coordinate.new(4,6), 1))
+    assert {:ok, %Island{coordinates: coordinates, hit_coordinates: _}} = isl
+    assert MapSet.size(coordinates) > 0
   end
 
-  test "cannot duplicate a hit_coordinate" do
-    isl = Island.new
-    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
-    assert MapSet.size(isl.hit_coordinates) == 1
+  test "can create :s_shape" do
+    isl = Island.new(:s_shape, elem(Coordinate.new(4,6), 1))
+    assert {:ok, %Island{coordinates: coordinates, hit_coordinates: _}} = isl
+    assert MapSet.size(coordinates) > 0
   end
+
+  test "can create :square" do
+    isl = Island.new(:square, elem(Coordinate.new(4,6), 1))
+    assert {:ok, %Island{coordinates: coordinates, hit_coordinates: _}} = isl
+    assert MapSet.size(coordinates) > 0
+  end
+
+  test "errors on invalid coordinates" do
+    isl = Island.new(:square, elem(Coordinate.new(10,10), 1))
+    assert {:error, :invalid_coordinate} = isl
+  end
+
+  test "errors on invalid shape" do
+    isl = Island.new(:unknown, elem(Coordinate.new(4,6), 1))
+    assert {:error, :invalid_island_type} = isl
+  end
+
+#  test "has hit_coordinates" do
+#    isl = Island.new
+#    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
+#    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(2,4), 1)))
+#    assert MapSet.size(isl.hit_coordinates) == 2
+#  end
+#
+#  test "cannot duplicate a coordinate" do
+#    isl = Island.new
+#    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
+#    isl = update_in(isl.coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
+#    assert MapSet.size(isl.coordinates) == 1
+#  end
+#
+#  test "cannot duplicate a hit_coordinate" do
+#    isl = Island.new
+#    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
+#    isl = update_in(isl.hit_coordinates, &MapSet.put(&1, elem(Coordinate.new(1,1), 1)))
+#    assert MapSet.size(isl.hit_coordinates) == 1
+#  end
 end
