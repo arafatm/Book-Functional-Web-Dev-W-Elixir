cb7061edaa446e3a57704458427b109c66a00515
Arafat M <github@arafatm.com>
2017-06-11 15:45:41 -0700

    Board.overlaps_existing_island?

--- islands_engine/model_data/lib/islands_engine/board.ex
+++ islands_engine/model_data/lib/islands_engine/board.ex
@@ -1,3 +1,12 @@
 defmodule IslandsEngine.Board do
+  alias IslandsEngine.Island
+
   def new(), do: %{}
+
+  # Enumerate over islands in board and check for overlap
+  defp overlaps_existing_island?(board, new_key, new_island) do
+    Enum.any?(board, fn {key, island} ->
+      key != new_key and Island.overlaps?(island, new_island)
+    end)
+  end
 end
