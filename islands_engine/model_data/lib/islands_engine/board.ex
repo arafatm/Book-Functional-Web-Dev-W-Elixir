d4eb60c7653d77af44bce5f3c593fbc075340c1b
Arafat M <github@arafatm.com>
2017-06-11 18:22:09 -0700

    Board.position_island(board, key %Island{} = island)

--- islands_engine/model_data/lib/islands_engine/board.ex
+++ islands_engine/model_data/lib/islands_engine/board.ex
@@ -3,6 +3,13 @@ defmodule IslandsEngine.Board do
 
 	def new(), do: %{}
 
+ 	def position_island(board, key, %Island{} = island) do
+ 	  case overlaps_existing_island?(board, key, island) do
+ 	    true  -> {:error, :overlapping_island}
+ 	    false -> Map.put(board, key, island)
+ 	  end
+ 	end
+
 	def all_islands_positioned?(board), do:
 	Enum.all?(Island.types, &(Map.has_key?(board, &1)))
 
